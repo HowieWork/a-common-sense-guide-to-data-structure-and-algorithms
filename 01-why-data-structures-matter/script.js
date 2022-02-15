6b710df62c820b652667577dfc62d4df25883bd2
Howie <howiewang2020@gmail.com>
2022-02-15 09:04:16 -0500

    Complete chapter 1, add chapter 2

--- 01-why-data-structures-matter/script.js
+++ 01-why-data-structures-matter/script.js
@@ -1,3 +1,6 @@
 'Use strict';
 
-
+// Book Exercises
+// 1. My answers: a. 1; b. 100; c. 101; d. 1; e. 100; f. 1 (Accuracy: 6/6; 100%)
+// 2. My answers: a. 1; b. 100; c. 201; d. 101; e. 100; f. 1 (Accuracy: 6/6; 100% )
+// 3. My answer: N (Accuracy:  1/1; 100%)
