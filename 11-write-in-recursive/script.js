57dce625787a55f6de7d3a4763cf647486ca6e07
Howie <howiewang2020@gmail.com>
2022-02-25 09:41:20 -0500

    complete chapter 11

--- 11-write-in-recursive/script.js
+++ 11-write-in-recursive/script.js
@@ -51,34 +51,6 @@ const possibleNumOfPaths = numOfStairs => {
 // console.log(possibleNumOfPaths(10));
 
 // NOTE 5. Anagram Generation
-function generateAnagrams(word) {
-  if (word.length < 2) {
-    return [word];
-  } else {
-    let anagrams = [];
-    let before, focus, after;
-    let shortWord, subAnagrams, newEntry;
-
-    for (let i = 0; i < word.length; i++) {
-      before = word.slice(0, i);
-      focus = word[i];
-      after = word.slice(i + 1, word.length + 1);
-      shortWord = before + after;
-      subAnagrams = generateAnagrams(shortWord);
-
-      for (let j = 0; j < subAnagrams.length; j++) {
-        newEntry = focus + subAnagrams[j];
-        anagrams.push(newEntry);
-      }
-    }
-
-    return anagrams;
-  }
-}
-
-// console.log(generateAnagrams('abc'));
-
-/*
 const anagramsGenerator = string => {
   // BASE CASE
   if (string.length === 1) return [string[0]];
@@ -88,9 +60,105 @@ const anagramsGenerator = string => {
 
   for (let i = 0; i < subAnagramsArray.length; i++) {
     // FOR EACH ITEM IN THIS SUB ANAGRAMS ARRAY, LOOP AGAIN!
+    for (let j = 0; j < subAnagramsArray[i].length + 1; j++) {
+      // INSERT THE FIRST CHAR OF PROVIDED STRING: string[0]
+      const newString =
+        subAnagramsArray[i].slice(0, j) +
+        string[0] +
+        subAnagramsArray[i].slice(j);
+      anagramsArray.push(newString);
+    }
+  }
+
+  return anagramsArray;
+};
+
+// console.log(anagramsGenerator('abc'));
+
+// Book Exercises (Accuracy: x/x xxx%)
+// 1. My answer:
+const getNumOfChar = array => {
+  let numOfChar;
+
+  // BASE CASE
+  if (array.length === 1) {
+    numOfChar = array[0].length;
+    return numOfChar;
+  }
+
+  // SUBPROBLEM
+  numOfChar = array[0].length + getNumOfChar(array.slice(1, array.length));
+
+  return numOfChar;
+};
+// console.log(getNumOfChar(['ab', 'c', 'def']));
+
+// 2. My answer:
+const getEvenNums = array => {
+  let evenNums = [];
+
+  // BASE CASE
+  if (array.length === 1) {
+    return array[0] % 2 === 0 ? array : [];
+  }
+
+  // SUBPROBLEM
+  evenNums = getEvenNums(array.slice(1));
+
+  if (array[0] % 2 === 0) {
+    evenNums.unshift(array[0]);
+  }
+
+  // RETURN AN ARRAY OF EVEN NUMBERS
+  return evenNums;
+};
+
+// console.log(getEvenNums([1, 2, 3, 4, 6]));
+
+// 3. 'Triangular Numbers' My answer:
+const getTriangularNum = n => {
+  let triangularNum;
+
+  // BASE CASE
+  if (n === 1) {
+    triangularNum = 1;
+  }
+
+  // SUBPROBLEM
+  if (n > 1) {
+    triangularNum = getTriangularNum(n - 1) + n;
+  }
+
+  return triangularNum;
+};
+// console.log(getTriangularNum(4));
+
+// 4. My answer:
+const getIndexOfFirstX = string => {
+  // BASE CASE
+  if (string.length === 1) {
+    return string[0] === 'x' ? 0 : null;
+  }
+
+  // SUBPROBLEM
+  if (string[0] === 'x') {
+    return 0;
+  } else {
+    return getIndexOfFirstX(string.slice(1, string.length)) + 1;
   }
 };
-*/
 
-// Book Exercises
-// X. My answer: (Accuracy: x/x xxx%)
+// console.log(getIndexOfFirstX('dabxcd'));
+
+// 5. 'Unique Paths' My answer:
+const getPathNum = (col, row) => {
+  // BASE CASE
+  if (col === 1 || row === 1) {
+    return 1;
+  }
+
+  // SUBPROBLEM
+  return getPathNum(col - 1, row) + getPathNum(col, row - 1);
+};
+
+// console.log(getPathNum(3, 3));
