c7b192ec757ee69c3029257efae5373b5a443082
Howie <howiewang2020@gmail.com>
2022-07-12 13:53:25 -0400

    add binary search tree implementation to playground

--- 00-playground/script.js
+++ 00-playground/script.js
@@ -1,4 +1,235 @@
 'Use strict';
 
 // NOTE Code Implementation
-console.log('Hello');
+// Binary Search Tree: code implementation
+// OPTION 1
+/*
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  // Average: T - O(logN) | S - O(logN)
+  // Worst: T - O(N) | S - O(N)
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+  // Average: T - O(logN) | S - O(logN)
+  // Worst: T - O(N) | S - O(N)
+  contains(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    } else {
+      return true;
+    }
+  }
+
+  // Average: T - O(logN) | S - O(logN)
+  // Worst: T - O(N) | S - O(N)
+  remove(value, parent = null) {
+    // 1. FIND WHERE THE TARGET VALUE IS
+    if (value < this.value) {
+      if (this.left !== null) {
+        this.left.remove(value, this);
+      }
+    } else if (value > this.value) {
+      if (this.right !== null) {
+        this.right.remove(value, this);
+      }
+    } else {
+      // 2. REMOVE THE TARGET VALUE
+      // 2.1 HAS TWO CHILD NODE
+      if (this.left !== null && this.right !== null) {
+        this.value = this.right.getMinValue();
+        this.right.remove(this.value, this);
+        // 2.2 HAS ONE CHILD NODE
+        // 2.2.1 TARGET NODE IS ROOT NODE
+      } else if (parent === null) {
+        if (this.left !== null) {
+          this.value = this.left.value;
+          this.right = this.left.right;
+          this.left = this.left.left;
+        } else if (this.right !== null) {
+          this.value = this.right.value;
+          this.left = this.right.left;
+          this.right = this.right.right;
+        } else {
+          // SINGLE NODE TREE, DO NOTHING!
+        }
+        // 2.2.2 TARGET NODE IS NOT ROOT NODE
+      } else if (parent.left === this) {
+        parent.left = this.left !== null ? this.left : this.right;
+      } else if (parent.right === this) {
+        parent.right = this.left !== null ? this.left : this.right;
+
+        // 2.3 HAS NO CHILD NODE
+      } else {
+        // SINGLE NODE TREE, DO NOTHING!
+      }
+    }
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+  getMinValue() {
+    if (this.left === null) {
+      return this.value;
+    } else {
+      return this.left.getMinValue();
+    }
+  }
+}
+
+const testingBST = new BST(10);
+testingBST.insert(5);
+testingBST.insert(15);
+testingBST.insert(2);
+testingBST.insert(5);
+testingBST.insert(13);
+testingBST.insert(1);
+testingBST.insert(22);
+testingBST.insert(14);
+
+console.log(testingBST);
+*/
+
+// OPTION 2
+// Node class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Binary Search tree class
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+    if (this.root === null) this.root = newNode;
+    else this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) node.left = newNode;
+      else this.insertNode(node.left, newNode);
+    } else {
+      if (node.right === null) node.right = newNode;
+      else this.insertNode(node.right, newNode);
+    }
+  }
+
+  remove(data) {
+    this.root = this.removeNode(this.root, data);
+  }
+
+  removeNode(node, key) {
+    if (node === null) {
+      return null;
+    } else if (key < node.data) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      // DELETING NODE WITH NO CHILDREN
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      // DELETING NODE WITH ONE CHILDREN
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // DELETING NODE WITH TWO CHILDREN
+      const aux = this.findMinNode(node.right);
+      node.data = aux.data;
+      node.right = this.removeNode(node.right, aux.data);
+      return node;
+    }
+  }
+
+  // Helper function
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+
+  inorder(node) {
+    if (node !== null) {
+      this.inorder(node.left);
+      console.log(node.data);
+      this.inorder(node.right);
+    }
+  }
+
+  preorder(node) {
+    if (node !== null) {
+      console.log(node.data);
+      this.preorder(node.left);
+      this.preorder(node.right);
+    }
+  }
+
+  postorder(node) {
+    if (node !== null) {
+      this.postorder(node.left);
+      this.postorder(node.right);
+      console.log(node.data);
+    }
+  }
+
+  search(node, data) {
+    if (node === null) return null;
+    else if (data < node.data) return this.search(node.left, data);
+    else if (data > node.data) return this.search(node.right, data);
+    else return node;
+  }
+}
